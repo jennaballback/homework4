ef3edc96a922ceb4be23009fa70b2cabf93f5535
jennaballback <jennaballback@gmail.com>
2021-10-14 18:23:24 -0400

    function3

--- homeowrk4.py
+++ homeowrk4.py
@@ -1,1 +1,13 @@
-print("Alt Tab")
+def myfunc3 ():
+  num = int(input("Enter a number: "))
+  num1 = int(input("Enter another number: "))
+  if ((num %2 ==0), (num1 %2 ==0)): #even
+    print (num, num1)
+    print (min(num, num1))
+  elif ((num %2 !=0), (num1 %2 !=0)): #odd
+    print ("hi")
+    print(max(num, num1))
+  else: 
+    print("hi")
+    #print (max(num, num1))
+myfunc3 ()
