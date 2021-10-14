29f4546c91314a52c738fe42ae4b23aff0f9a200
Dylan Ballback <44906769+dylanballback@users.noreply.github.com>
2021-10-14 18:23:20 -0400

    updated func

--- homeowrk4.py
+++ homeowrk4.py
@@ -1,1 +1,22 @@
-print("Alt Tab")
+def myfun3(numbers):
+    """This is where i describe what the function does.
+    
+    Args:
+        numbers: Numbers entered by the user.
+    
+    """
+
+    if (numbers[0] %2 == 0) and (numbers[1] %2 == 0) : # Even
+        print(numbers)
+        print(min(numbers))
+    elif (numbers[0] %2 != 0) and (numbers[1] %2 != 0): # Odd
+        print(numbers)
+        print(max(numbers))
+    else:
+        print("I dont like numbers")
+
+number1 = int(input("Enter first number:"))
+number2 = int(input("Enter second number:"))
+numbers = [number1, number2]
+
+myfun3(numbers)
