bf49119652d238594495c71956a54986f506ef3b
jennaballback <jennaballback@gmail.com>
2021-11-16 15:17:37 -0500

    new

--- homeowrk4.py
+++ homeowrk4.py
@@ -1,31 +1,9 @@
-'''def myfun3(numbers):
-    """This is where i describe what the function does.
-    
-    Args:
-        numbers: Numbers entered by the user.
-    
-    """
-
-    if (numbers[0] %2 == 0) and (numbers[1] %2 == 0) : # Even
-        print(numbers)
-        print(min(numbers))
-    elif (numbers[0] %2 != 0) and (numbers[1] %2 != 0): # Odd
-        print(numbers)
-        print(max(numbers))
-    else:
-        print("I dont like numbers")
-
-number1 = int(input("Enter first number:"))
-number2 = int(input("Enter second number:"))
-numbers = [number1, number2]
-
-myfun3(numbers)'''
-
-def myfunc4 ():
-  name = str(input("Enter two words: "))
-  lit = name.split()
-  if ((list(lit[0])[0]) == (list(lit[1])[0])):
-    print("TRUE")
-  else:
-    print("False")
-myfunc4()
+def myfunc2 (blackjack, black, jack):
+    print(blackjack,black,jack)
+    sum = blackjack+black+jack
+    if sum <= 21:
+        print(sum)
+    elif sum > 21:
+        nsum = sum - 10
+        print(nsum)
+print(myfunc2 (1,3,2))
