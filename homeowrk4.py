5df9c721d8eb6e48854a71b7eb125918a21e6ac7
jennaballback <jennaballback@gmail.com>
2021-11-16 16:38:03 -0500

    done

--- homeowrk4.py
+++ homeowrk4.py
@@ -1,9 +1,15 @@
 def myfunc2 (blackjack, black, jack):
     print(blackjack,black,jack)
     sum = blackjack+black+jack
-    if sum <= 21:
+    if blackjack == 11 or black == 11 or jack == 11 and sum > 21:
+        sum1 = sum - 10
+        print(sum1)
+    elif sum <= 21:
         print(sum)
     elif sum > 21:
-        nsum = sum - 10
-        print(nsum)
-print(myfunc2 (1,3,2))
+        print("Bust")
+    else:
+        print("Error")
+print(myfunc2 (11,3,2))
+print(myfunc2 (1,2,3))
+print(myfunc2(10,9,9))
